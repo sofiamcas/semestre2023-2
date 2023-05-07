b74f5e1cf700c4f1ac05e7209d2015b0b6801f8f
sofiamcas <119833805+sofiamcas@users.noreply.github.com>
2023-05-07 13:17:13 -0600

    Add files via upload

---
+++ LayoutsJavaActExtra3/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //Ventana2 ventana = new Ventana2("Layouts");
+        VentanaGrid2 ventanaGrid = new VentanaGrid2("Ventana Layout cuadrada");
+    }
+}
