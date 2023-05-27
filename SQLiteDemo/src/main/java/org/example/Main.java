7afb41d15fdb028ca44acd9ea732d31e062d548f
sofiamcas <119833805+sofiamcas@users.noreply.github.com>
2023-05-27 13:39:09 -0600

    Add files via upload

--- SQLiteDemo/src/main/java/org/example/Main.java
+++ SQLiteDemo/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
+import org.example.controlador.ControladorLibro;
+import org.example.modelo.Libro;
+import org.example.persistencia.LibroDAO;
+import org.example.vista.VentanaLibro;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        VentanaLibro view = new VentanaLibro("MVC Y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
     }
 }
